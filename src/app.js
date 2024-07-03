2beb1c5f4156c2cad76a91cfeb7bca16a7f6ef89
Talha1725 <talhanoor1001@gmail.com>
2024-07-03 21:01:12 +0500

    Added get-field-book-id route and file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const getFieldBookByIDs = require('./routes/getFieldBookByIDs');
 const farmerBookRoutes = require('./routes/farmerBookRoutes');
 const fieldBookRoutes = require('./routes/fieldBookRoutes');
 const analysisRoutes = require('./routes/analysisRoutes');
+const FarmerIDs = require('./routes/farmer_savi_ids');
 const getBookByID = require('./routes/getBookByIDs');
 const AddField = require('./routes/addField');
-const FarmerIDs = require('./routes/farmer_savi_ids');
 
 
 const app = express();
@@ -15,6 +16,7 @@ app.use('/farmer-book', farmerBookRoutes);
 app.use('/field-book', fieldBookRoutes);
 app.use('/analysis', analysisRoutes);
 app.use('/get-all-books', getBookByID);
+app.use('/get-field-book-id', getFieldBookByIDs);
 app.use('/add-field', AddField);
 app.use('/get-all-farmer-id', FarmerIDs);
 
